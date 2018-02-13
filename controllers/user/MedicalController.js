8a3f508a5a2c910b34d3d1d48f70c6bd4bfb504a
HIMANSHU RAJPUT <himanshurajput56@gmail.com>
2018-02-13 13:41:08 +0000

    him

---
+++ controllers/user/MedicalController.js
@@ -0,0 +1,26 @@
+'use strict';
+const
+    path = require('path'),
+    _  = require('lodash'),
+    async = require('async'),
+    mongoose  = require('mongoose');
+var medDetINS = require(path.resolve('./models/medicalModel'));
+
+exports.MedicalDetIns = function (req, res, next ) {
+    var medField = {
+        Text:req.body.Text,
+        Description:req.body.Description
+    }
+    var MedicalDI = new medDetINS(medField);
+    medicalDI.save(function (err, saveObj) {
+       if(err){
+           res.json({obj:err,message:'Data Is Not Saved'});
+       } else {
+           res.json({obj:saveObj,message:'Data Saved Successfully'});
+       }
+    });
+}
+
+
+
+
